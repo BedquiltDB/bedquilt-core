4c00a8df64c6c4112586c69f182e2dc37a2ba27c
Shane Kilkelly <shane@kilkelly.me>
2015-02-28 11:38:39 +0000

    more things

--- sql/002-generate-id.sql
+++ sql/002-generate-id.sql
@@ -1,5 +1,7 @@
-CREATE OR REPLACE FUNCTION bq_generate_id () RETURNS text AS $$
+DROP FUNCTION IF EXISTS bq_generate_id();
+
+CREATE OR REPLACE FUNCTION bq_generate_id () RETURNS char(24) AS $$
 BEGIN
-  RETURN CAST(encode(gen_random_bytes(12), 'hex') as text);
+  RETURN CAST(encode(gen_random_bytes(12), 'hex') as char(24));
 END
 $$ LANGUAGE plpgsql IMMUTABLE STRICT;
