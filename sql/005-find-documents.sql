b1731d0930e08c14c82a6c10e875d52010995f21
Shane Kilkelly <shane@kilkelly.me>
2015-03-05 22:46:29 +0000

    return nothing if the table does not exist

--- sql/005-find-documents.sql
+++ sql/005-find-documents.sql
@@ -6,13 +6,16 @@ CREATE OR REPLACE FUNCTION bq_findone_document(
 ) RETURNS table(jdoc json) AS $$
 BEGIN
 
-EXECUTE format(
-    'SELECT payload FROM %I
-     WHERE payload @> (''%s'')::jsonb
-     LIMIT 1',
-     i_coll,
-     i_json_query
- );
+IF EXISTS (SELECT relname FROM pg_class WHERE relname = format('%s', i_coll))
+THEN
+    RETURN QUERY EXECUTE format(
+        'SELECT payload::json FROM %I
+        WHERE payload @> (''%s'')::jsonb
+        LIMIT 1',
+        i_coll,
+        i_json_query
+    );
+END IF;
 
 END
 $$ LANGUAGE plpgsql;
