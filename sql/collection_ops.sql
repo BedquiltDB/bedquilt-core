fe293f1b5f4323aba2d23e4cd9e1e48bfa41f444
Shane Kilkelly <shane@kilkelly.me>
2015-03-08 17:52:40 +0000

    fix things

--- sql/collection_ops.sql
+++ sql/collection_ops.sql
@@ -16,6 +16,7 @@ THEN
       updated timestamptz default current_timestamp,
       CONSTRAINT validate_id CHECK ((jdoc->>''_id'') IS NOT NULL)
   );
+  COMMENT ON TABLE %1$I IS ''bedquilt.%1$s'';
   CREATE INDEX idx_%1$I_jdoc ON %1$I USING gin (jdoc);
   CREATE UNIQUE INDEX idx_%1$I_jdoc_id ON %1$I ((jdoc->>''_id''));
   ', i_coll);
@@ -30,11 +31,11 @@ CREATE OR REPLACE FUNCTION bq_list_collections()
 RETURNS table(collection_name text) AS $$
 BEGIN
 
-return query select table_name::text
-from information_schema.tables
-where table_schema = 'public'
-and pg_catalog.obj_description((table_name)::regclass, 'pg_class')
-like 'bedquilt.%';
+RETURN QUERY SELECT table_name::text
+       FROM information_sachema.tables
+       WHERE table_schema = 'public'
+       AND pg_catalog.obj_description((table_name)::regclass, 'pg_class')
+       LIKE 'bedquilt.%';
 
 END
 $$ LANGUAGE plpgsql;
