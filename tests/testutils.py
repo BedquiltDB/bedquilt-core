ed350a7387c376538f51a8a7a8cfde5469baba8a
Shane Kilkelly <shane@kilkelly.me>
2015-03-13 23:43:52 +0000

    Add the sql to create the test database

--- tests/testutils.py
+++ tests/testutils.py
@@ -3,6 +3,15 @@ import os
 import getpass
 
 
+# CREATE DATABASE bedquilt_test
+#   WITH OWNER = {{owner}}
+#        ENCODING = 'UTF8'
+#        TABLESPACE = pg_default
+#        LC_COLLATE = 'en_GB.UTF-8'
+#        LC_CTYPE = 'en_GB.UTF-8'
+#        CONNECTION LIMIT = -1;
+
+
 def get_pg_connection():
     return psycopg2.connect(
         "dbname=bedquilt_test user={}".format(getpass.getuser())
