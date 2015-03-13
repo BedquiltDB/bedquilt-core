312d81502c37d9f5a57e2ad6d2960de58826bbd3
Shane Kilkelly <shane@kilkelly.me>
2015-03-13 23:48:18 +0000

    Change params

--- tests/testutils.py
+++ tests/testutils.py
@@ -14,5 +14,6 @@ import getpass
 
 def get_pg_connection():
     return psycopg2.connect(
-        "dbname=bedquilt_test user={}".format(getpass.getuser())
+        database='bedquilt_test',
+        user=getpass.getuser()
     )
