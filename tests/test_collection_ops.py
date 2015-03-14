4a35b13b11a4461f6731c3714275b7fce4fda408
Shane Kilkelly <shane@kilkelly.me>
2015-03-14 11:09:03 +0000

    more tests

--- tests/test_collection_ops.py
+++ tests/test_collection_ops.py
@@ -6,20 +6,36 @@ class TestCreateCollection(unittest.TestCase):
 
     def setUp(self):
         self.conn = testutils.get_pg_connection()
+        self.cur = self.conn.cursor()
+        testutils.clean_database(self.conn)
 
     def test_creating_a_new_collection(self):
-        cur = self.conn.cursor()
-
         # create a collection 'testone'
-        cur.execute("select bq_create_collection('testone')")
-        result = cur.fetchone()
+        self.cur.execute("select bq_create_collection('testone')")
+        result = self.cur.fetchone()
 
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0], True)
 
         # create it again
-        cur.execute("select bq_create_collection('testone')")
-        result = cur.fetchone()
+        self.cur.execute("select bq_create_collection('testone')")
+        result = self.cur.fetchone()
 
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0], False)
+
+
+class TestListCollections(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = testutils.get_pg_connection()
+        self.cur = self.conn.cursor()
+
+        testutils.clean_database(self.conn)
+
+    def test_list_collections_empty_instance(self):
+
+        self.cur.execute("select bq_list_collections();")
+        result = self.cur.fetchone()
+
+        self.assertIsNone(result)
