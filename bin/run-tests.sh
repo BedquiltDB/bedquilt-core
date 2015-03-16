476fa730fe3da39540a89659e3dbdc04dda6f406
Shane Kilkelly <shane@kilkelly.me>
2015-03-16 20:11:54 +0000

    update test script

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/env sh
 
+
+echo ">> Installing latest version of bedquilt to localhost"
+
+
+make install
+psql -d bedquilt_test \
+     -c "create extension if not exists pgcrypto;
+         drop extension if exists bedquilt;
+         create extension bedquilt;"
+
+
 echo ">> Running tests"
 python -m unittest discover test
