90e7f396d8f9c3bf48a87ed3916680e24e118030
Shane Kilkelly <shane@kilkelly.me>
2015-03-16 21:43:56 +0000

    more tests

--- test/test_collection_ops.py
+++ test/test_collection_ops.py
@@ -28,12 +28,39 @@ class TestListCollections(testutils.BedquiltTestCase):
 
         self.assertIsNone(result)
 
+    def test_list_four_collections(self):
+        self.cur.execute("""
+        select bq_create_collection('c_one');
+        select bq_create_collection('c_two');
+        select bq_create_collection('c_three');
+        select bq_create_collection('c_four');
+        """, )
+
+        self.cur.execute("select bq_list_collections();")
+        result = self.cur.fetchall()
+
+        self.assertIsNotNone(result)
+
+        self.assertEqual(map(lambda e: e[0], result),
+                         ['c_one', 'c_two', 'c_three', 'c_four'])
+
 
 class TestDeleteCollection(testutils.BedquiltTestCase):
 
-    def test_list_collections_empty_instance(self):
+    def test_delete_non_existant_collection(self):
 
-        self.cur.execute("select bq_list_collections();")
+        self.cur.execute("select bq_delete_collection('one');")
         result = self.cur.fetchone()
 
-        self.assertIsNone(result)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0], False)
+
+    def test_delete_collection(self):
+
+        self.cur.execute("select bq_create_collection('one')")
+
+        self.cur.execute("select bq_delete_collection('one');")
+        result = self.cur.fetchone()
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0], True)
