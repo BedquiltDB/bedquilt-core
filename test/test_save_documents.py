ef35c4c0c8f0d0b9eee0f17594058cf0b7eb9663
Shane Kilkelly <shane@kilkelly.me>
2015-03-28 11:11:21 +0000

    Refactor the tests to use _query

--- test/test_save_documents.py
+++ test/test_save_documents.py
@@ -7,39 +7,33 @@ import psycopg2
 class TestSaveDocuments(testutils.BedquiltTestCase):
 
     def test_save_into_non_existant_collection(self):
-        self.cur.execute("""
+        _ = self._query("""
         select bq_save('things', '{"_id": "aaa", "a": 1}');
         """)
-        result = self.cur.fetchall()
 
-        self.cur.execute("""
+        result = self._query("""
         select bq_find('things', '{}');
         """)
-        result = self.cur.fetchall()
 
         self.assertEqual(result,
                          [({'_id': 'aaa', 'a': 1},)])
 
     def test_save_with_no_id(self):
-        self.cur.execute("""
+        _ = self._query("""
         select bq_create_collection('thing');
         """)
-        _ = self.cur.fetchall()
 
         doc = {
             "name": "spanner"
         }
 
-        self.cur.execute("""
+        _ = self._query("""
         select bq_save('things', '{}');
         """.format(json.dumps(doc)))
 
-        _ = self.cur.fetchall()
-
-        self.cur.execute("""
+        result = self._query("""
         select bq_find('things', '{}');
         """)
-        result = self.cur.fetchall()
 
         self.assertEqual(len(result), 1)
         d = result[0][0]
@@ -59,16 +53,12 @@ class TestSaveDocuments(testutils.BedquiltTestCase):
             "name": "spanner"
         }
 
-        self.cur.execute("""
-        select bq_insert('things', '{}');
-        """.format(json.dumps(doc)))
-
+        self._insert('things', doc)
         _ = self.cur.fetchall()
 
-        self.cur.execute("""
+        result = self._query("""
         select bq_find('things', '{}');
         """)
-        result = self.cur.fetchall()
 
         self.assertEqual(result,
                          [
@@ -78,16 +68,13 @@ class TestSaveDocuments(testutils.BedquiltTestCase):
         doc['name'] = 'fish'
         doc['color'] = 'blue'
 
-        self.cur.execute("""
+        self._query("""
         select bq_save('things', '{}');
         """.format(json.dumps(doc)))
 
-        _ = self.cur.fetchall()
-
-        self.cur.execute("""
+        result = self._query("""
         select bq_find('things', '{}');
         """)
-        result = self.cur.fetchall()
 
         self.assertEqual(result,
                          [
