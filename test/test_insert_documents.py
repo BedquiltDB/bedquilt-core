324c326984019f4a4bbfe85a692d52282e1ab2a8
Shane Kilkelly <shane@kilkelly.me>
2015-03-28 12:06:18 +0000

    Better tests for ensuring _id is a string

--- test/test_insert_documents.py
+++ test/test_insert_documents.py
@@ -30,17 +30,38 @@ class TestInsertDocument(testutils.BedquiltTestCase):
         self.assertEqual(collections, [("people",)])
 
     def test_with_non_string_id(self):
-        doc = {
-            "_id": 42,
-            "name": "Penguin",
-            "age": "penguin@example.com"
-        }
-
-        with self.assertRaises(psycopg2.InternalError):
-            self.cur.execute("""
-            select bq_insert('people', '{}');
-            """.format(json.dumps(doc)))
-        self.conn.rollback()
+        docs = [
+            {
+                "_id": 42,
+                "name": "Penguin",
+                "age": "penguin@example.com"
+            },
+            {
+                "_id": ['derp'],
+                "name": "Penguin",
+                "age": "penguin@example.com"
+            },
+            {
+                "_id": {"name": "Penguin"},
+                "age": "penguin@example.com"
+            },
+            {
+                "_id": False,
+                "name": "Penguin",
+                "age": "penguin@example.com"
+            },
+            {
+                "_id": None,
+                "name": "Penguin",
+                "age": "penguin@example.com"
+            }
+        ]
+        for doc in docs:
+            with self.assertRaises(psycopg2.InternalError):
+                self.cur.execute("""
+                select bq_insert('people', '{}');
+                """.format(json.dumps(doc)))
+            self.conn.rollback()
 
     def test_insert_without_id(self):
         doc = {
