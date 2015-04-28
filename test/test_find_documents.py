493ca7bc02b99df94fa8061bf5229064aa533a99
Shane Kilkelly <shane@kilkelly.me>
2015-04-28 19:12:35 +0100

    [#5] Test with a large number of docs

--- test/test_find_documents.py
+++ test/test_find_documents.py
@@ -29,6 +29,17 @@ class TestCollectionCount(testutils.BedquiltTestCase):
         """)
         self.assertEqual(result, [(1,)])
 
+    def test_count_with_many_docs(self):
+        for i in range(100):
+            _ = self._query("""
+            select bq_insert('things', '{"a": 1}')
+            """)
+
+        result = self._query("""
+        select bq_count('things')
+        """)
+        self.assertEqual(result, [(100,)])
+
 class TestFindDocuments(testutils.BedquiltTestCase):
 
     def test_find_on_empty_collection(self):
