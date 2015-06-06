0d3140a3693954bfe6c7d58165b1d0cdbe7c2d9f
Shane Kilkelly <shane@kilkelly.me>
2015-06-06 14:48:18 +0100

    Test the $notNull constraint.

--- test/test_collection_ops.py
+++ test/test_collection_ops.py
@@ -36,7 +36,56 @@ class TestConstraints(testutils.BedquiltTestCase):
         result = self._query("""
         select bq_insert('things', '{}')
         """.format(json.dumps(doc)))
+        self.assertIsNotNone(result)
 
+        # should be fine with a name key, even null
+        doc = {
+            'name': None,
+            'age': 24
+        }
+        result = self._query("""
+        select bq_insert('things', '{}')
+        """.format(json.dumps(doc)))
+
+        self.assertIsNotNone(result)
+
+    def test_notNull_constraint(self):
+        result = self._query("""
+        select bq_add_constraint('things', '{}');
+        """.format(json.dumps({
+            'name': {'$notNull': 1}
+        })))
+        self.assertEqual(result, [(True,)])
+
+        # should reject doc with name missing
+        doc = {
+            'age': 24
+        }
+        with self.assertRaises(psycopg2.IntegrityError):
+            self._query("""
+            select bq_insert('things', '{}');
+            """.format(json.dumps(doc)))
+        self.conn.rollback()
+
+        # should reject doc with name set to null
+        doc = {
+            'name': None,
+            'age': 24
+        }
+        with self.assertRaises(psycopg2.IntegrityError):
+            self._query("""
+            select bq_insert('things', '{}');
+            """.format(json.dumps(doc)))
+        self.conn.rollback()
+
+        # should be fine with a name key that is not null
+        doc = {
+            'name': 'steve',
+            'age': 24
+        }
+        result = self._query("""
+        select bq_insert('things', '{}')
+        """.format(json.dumps(doc)))
         self.assertIsNotNone(result)
 
 
