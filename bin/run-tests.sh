fefeb6d7126f18e3f4d635aa60c8e038fe86b262
Shane Kilkelly <shane@kilkelly.me>
2015-06-09 21:19:18 +0100

    If we can't install the bedquilt package, don't run the tests.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -7,6 +7,11 @@ psql -d bedquilt_test \
      -c "create extension if not exists pgcrypto;
          drop extension if exists bedquilt;
          create extension bedquilt;"
+if [ $? -ne 0 ]
+then
+  echo ">> Error while installing bedquilt, exiting..."
+  exit 1
+fi
 
 
 echo ">> Running tests..."
