30c435aa3cc954633bd1eb10a916844653f766a0
Shane Kilkelly <shane@kilkelly.me>
2016-08-01 18:23:43 +0100

    Start testing query-splitter

--- src/sql/utilities.sql
+++ src/sql/utilities.sql
@@ -192,46 +192,42 @@ AS $$
 
     data = json.loads(i_json)
     special_queries = []
-    if 'proc' in SD:
-        proc = SD['proc']
-    else:
-        def proc(d, current_path):
-            keys = d.keys()
-            deletions = []
-            for k in keys:
-                v = d[k]
-                if k.startswith('$'):
-                    op = None
-                    if k == '$eq':
-                        op = "="
-                    if k == '$ne':
-                        op = "!="
-                    elif k == '$gte':
-                        op = ">="
-                    elif k == '$gt':
-                        op = ">"
-                    elif k == '$lte':
-                        op = "<="
-                    elif k == '$lt':
-                        op = "<"
-                    if op is None:
-                        plpy.fatal("Invalid query operator: {}".format(k))
-                    special_queries.append(
-                        """
-                        and bq_jdoc #> '{{{}}}'  {}  '{}'::jsonb
-                        """.format(",".join(current_path), op, json.dumps(v)).strip()
-                     )
-                    deletions.append(k)
-                else:
-                    if type(v) == dict:
-                        p = current_path.copy()
-                        p.extend([k])
-                        proc(v, p)
-                        if len(v.keys()) == 0:
-                            deletions.append(k)
-            for s in deletions:
-                del d[s]
-        SD['proc'] = proc
+    def proc(d, current_path):
+        keys = d.keys()
+        deletions = []
+        for k in keys:
+            v = d[k]
+            if k.startswith('$'):
+                op = None
+                if k == '$eq':
+                    op = "="
+                if k == '$ne':
+                    op = "!="
+                elif k == '$gte':
+                    op = ">="
+                elif k == '$gt':
+                    op = ">"
+                elif k == '$lte':
+                    op = "<="
+                elif k == '$lt':
+                    op = "<"
+                if op is None:
+                    plpy.fatal("Invalid query operator: {}".format(k))
+                special_queries.append(
+                    """
+                    and bq_jdoc #> '{{{}}}' {} '{}'::jsonb
+                    """.format(",".join(current_path), op, json.dumps(v)).strip()
+                  )
+                deletions.append(k)
+            else:
+                if type(v) == dict:
+                    p = current_path.copy()
+                    p.extend([k])
+                    proc(v, p)
+                    if len(v.keys()) == 0:
+                        deletions.append(k)
+        for s in deletions:
+            del d[s]
     proc(data, [])
     return (json.dumps(data), special_queries)
 $$ LANGUAGE plpython3u;
---
+++ test/test_utility_functions.py
@@ -0,0 +1,67 @@
+import testutils
+import json
+import psycopg2
+
+
+class TestSplitQueries(testutils.BedquiltTestCase):
+
+    def test_simple_queries(self):
+
+        examples = [
+            ({'a': {'b': 1}},
+             {'a': {'b': 1}},
+             []),
+            ({'a': 1, 'b': 2},
+             {'a': 1, 'b': 2},
+             [])
+        ]
+
+        for query, match, specials in examples:
+            result = self._query("""
+            select * from bq_split_queries('{}'::jsonb)
+            """.format(json.dumps(query)))
+            self.assertEqual(json.loads(result[0][0]), match)
+            self.assertEqual(result[0][1], specials)
+
+    def test_advanced_queries(self):
+        examples = [
+            (
+                {
+                    'a': {
+                        'b': {
+                            '$eq': 22
+                        }
+                    }
+                },
+                {},
+                ["and bq_jdoc #> '{a,b}' = '22'::jsonb"]
+            ),
+            (
+                {
+                    'a': {
+                        'b': {
+                            '$eq': 22
+                        }
+                    },
+                    'c': 44
+                },
+                {
+                    'c': 44
+                },
+                ["and bq_jdoc #> '{a,b}' = '22'::jsonb"]
+            )
+        ]
+
+        for query, match, specials in examples:
+            result = self._query("""
+            select * from bq_split_queries('{}'::jsonb)
+            """.format(json.dumps(query)))
+
+            self.assertEqual(
+                json.loads(result[0][0]),
+                match
+            )
+            self.assertEqual(
+                result[0][1],
+                specials
+            )
