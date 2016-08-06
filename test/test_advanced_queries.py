ceef03969af290a461f20e251b4c4ec1c9c8abeb
Shane Kilkelly <shane@kilkelly.me>
2016-08-06 17:58:15 +0100

    test $gte and $lte

--- test/test_advanced_queries.py
+++ test/test_advanced_queries.py
@@ -35,7 +35,7 @@ class TestAdvancedQueries(testutils.BedquiltTestCase):
         )
         self.assertEqual(result[0][0]['label'], 'f')
 
-    def test_gt(self):
+    def test_gt_and_gte(self):
         rows = [
             {"_id": "aa", "label": "a", "n": 1,  "color": "red"},
             {"_id": "bb", "label": "b", "n": 4,  "color": "red"},
@@ -64,3 +64,49 @@ class TestAdvancedQueries(testutils.BedquiltTestCase):
             }))
         )
         self.assertEqual(result[0][0]['label'], 'c')
+
+        result = self._query(
+            "select bq_find_one('things', '{}')".format(json.dumps({
+                'color': 'red',
+                'n': {'$gte': 8},
+            }))
+        )
+        self.assertEqual(result[0][0]['label'], 'c')
+
+    def test_lt(self):
+        rows = [
+            {"_id": "aa", "label": "a", "n": 1,  "color": "red"},
+            {"_id": "bb", "label": "b", "n": 4,  "color": "red"},
+            {"_id": "cc", "label": "c", "n": 8,  "color": "red"},
+            {"_id": "dd", "label": "d", "n": 16, "color": "blue"},
+            {"_id": "ee", "label": "e", "n": 8,  "color": "blue"},
+            {"_id": "ff", "label": "f", "n": 16, "color": "red"},
+            {"_id": "dud", "color": "blue"}
+        ]
+        for row in rows:
+            self._insert('things', row)
+
+        # find_one
+        result = self._query(
+            "select bq_find_one('things', '{}')".format(json.dumps({
+                'color': 'blue',
+                'n': {'$lt': 10},
+            }))
+        )
+        self.assertEqual(result[0][0]['label'], 'e')
+
+        result = self._query(
+            "select bq_find_one('things', '{}')".format(json.dumps({
+                'color': 'red',
+                'n': {'$lt': 2},
+            }))
+        )
+        self.assertEqual(result[0][0]['label'], 'a')
+
+        result = self._query(
+            "select bq_find_one('things', '{}')".format(json.dumps({
+                'color': 'blue',
+                'n': {'$lte': 8},
+            }))
+        )
+        self.assertEqual(result[0][0]['label'], 'e')
