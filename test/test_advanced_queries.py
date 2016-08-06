5edf01db4ba2fe2880bc757b90b0ab4f17baf531
Shane Kilkelly <shane@kilkelly.me>
2016-08-06 21:53:22 +0100

    Test $notin

--- test/test_advanced_queries.py
+++ test/test_advanced_queries.py
@@ -166,3 +166,33 @@ class TestAdvancedQueries(testutils.BedquiltTestCase):
             }))
         )
         self.assertEqual(result[0][0]['label'], 'e')
+
+    def test_notin(self):
+        rows = [
+            {"_id": "aa", "label": "a", "n": 1,  "color": "red"},
+            {"_id": "bb", "label": "b", "n": 4,  "color": "red"},
+            {"_id": "cc", "label": "c", "n": 8,  "color": "red"},
+            {"_id": "dd", "label": "d", "n": 16, "color": "blue"},
+            {"_id": "ee", "label": "e", "n": 8,  "color": "blue"},
+            {"_id": "ff", "label": "f", "n": 16, "color": "red"},
+            {"_id": "dud", "color": "blue"}
+        ]
+        for row in rows:
+            self._insert('things', row)
+
+        # find_one
+        result = self._query(
+            "select bq_find_one('things', '{}')".format(json.dumps({
+                'color': 'red',
+                'n': {'$notin': [1, 8, 16]},
+            }))
+        )
+        self.assertEqual(result[0][0]['label'], 'b')
+
+        result = self._query(
+            "select bq_find_one('things', '{}')".format(json.dumps({
+                'color': 'blue',
+                'n': {'$notin': [16, 12]},
+            }))
+        )
+        self.assertEqual(result[0][0]['label'], 'e')
